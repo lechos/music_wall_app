38a13ee0f4fcefce85f5b6e8d2475a9e2cde5eab
lechos <linneasaltel@gmail.com>
2016-04-11 18:45:18 +0000

    made modal for posting a song

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+ $(document).ready(function() {
+
 
   var target = $('#songs_header');
   var targetHeight = target.outerHeight();
@@ -9,5 +10,23 @@ $(document).ready(function() {
       target.css('opacity', scrollPercent);
     }
   }); 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+ 
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+  });
 });
